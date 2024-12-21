714d1866b51e2e1b7c3683003c057f65155f9467
eagle-edo-dsbm <dsbmmusicguitar@gmail.com>
2024-12-21 07:06:43 +0200

    *shot*

---
+++ js/header_loader1.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    fetch('header.html')
+    .then(response => {
+        if(!response.ok) throw new Error('Ошибка загрузки хедера!');
+        return response.text();
+    })
+    .then(html => {
+        document.body.insertAdjacentHTML('afterbegin', html);
+    })
+    .catch(error => console.error(error));
+})
